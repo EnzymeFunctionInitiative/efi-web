eb14bbea18cb4592355f6bf00c06b437686520a5
Nils Oberg <noberg@illinois.edu>
2018-05-03 14:54:38 -0500

    Global style and shared lib update.

--- includes/login_check.inc.php
+++ includes/login_check.inc.php
@@ -4,10 +4,13 @@
 require_once(__DIR__ . "/../libs/user_auth.class.inc.php");
 
 $IsLoggedIn = false;
+$IsAdminUser = false;
 if (user_auth::has_token_cookie()) {
     $userEmail = user_auth::get_email_from_token($db, user_auth::get_user_token());
     if ($userEmail)
         $IsLoggedIn = $userEmail;
+
+    $IsAdminUser = user_auth::get_user_admin($db, $userEmail);
 }
 ?>
 
