8acc179eb048e36f4375e8a42671102ac1f2d376
Nils Oberg <noberg@illinois.edu>
2018-07-26 10:43:52 -0500

    Updated ShortBRED heatmap UI.

--- shortbred/html/heatmap.php
+++ shortbred/html/heatmap.php
@@ -56,8 +56,21 @@ var colors5 = [
     'rgb(253, 231, 37)',
 ];
 
+var mgColors = [
+    "RoyalBlue",
+    "BurlyWood",
+    "Gray",
+    "Turquoise",
+    "SeaGreen",
+    "Orange",
+    "Salmon",
+    "Purple",
+    "LightSteelBlue",
+];
+
 var Id = <?php echo $_GET["id"]; ?>;
 var Key = "<?php echo $_GET["key"]; ?>";
+var ResType = "<?php echo $_GET["res"]; ?>";
 
 linspace = function (a,b,n) {
     if(typeof n === "undefined") n = Math.max(Math.round(b-a)+1,1);
@@ -71,23 +84,62 @@ linspace = function (a,b,n) {
 var processData = function(data) {
 
     var logScale = true;
-    var minLog = 4;
-    var minLogVal = Math.pow(10, -minLog);
+
+    var convertRawValue = function(rawVal) {
+        if (!logScale) {
+            return rawVal;
+        } else {
+            var minLog = 4;
+            var minLogVal = Math.pow(10, -minLog);
+            var val;
+            if (rawVal >= minLogVal)
+                val = Math.log10(rawVal);
+            else
+                val = -minLog;
+            var posVal = minLog + val;
+            return posVal;
+        }
+    }
 
     var numMetagenomes = data.metagenomes.length;
     var numClusters = data.clusters.length;
 
+    var min = data.extent.min;
+    var max = data.extent.max;
+
+    var useManualExtent = false;
+    if (min < 100000 && max > -100000) { // Is valid?
+        useManualExtent = true;
+        min = convertRawValue(min);
+        max = convertRawValue(max);
+    }
+
     var x = [],
         y = linspace(1, numClusters),
         z = [],
-        bodysite = [],
+        bodysiteMgLabels = [],
         gender = [],
         label = [];
+    var regions = [];
+    var lastBodysite = "";
+    var lastBodysiteIndex = 0;
     for (var i = 0; i < numMetagenomes; i++) {
-        x.push(data.metagenomes[i]);
-        bodysite.push(data.site_info.site[data.metagenomes[i]]);
-        gender.push(data.site_info.gender[data.metagenomes[i]]);
+        var mg = data.metagenomes[i];
+        var bodysite = data.site_info.site[mg];
+        x.push(mg);
+        bodysiteMgLabels.push(bodysite);
+        gender.push(data.site_info.gender[mg]);
+
+        if (i == 0)
+            lastBodysite = bodysite;
+        if (bodysite != lastBodysite) {
+            regions.push([lastBodysiteIndex, i, lastBodysite]);
+            lastBodysite = bodysite;
+            lastBodysiteIndex = i + 1;
+        }
     }
+
+    regions.push([lastBodysiteIndex, numMetagenomes-1, bodysiteMgLabels[numMetagenomes-1]]);
 
     for (var i = 0; i < numClusters; i++) {
         z.push([]);
@@ -96,20 +148,9 @@ var processData = function(data) {
         for (var j = 0; j < numMetagenomes; j++) {
             var rawVal = data.clusters[i].abundance[j];
             // <0.000001 = -6, 0.00001 = -5, 0.0001 = -4, 0.001 = -3, 0.01 = -2, 0.1 = -1, 1 = 0
-            if (logScale) {
-                var val;
-                if (rawVal >= minLogVal)
-                    val = Math.log10(rawVal);
-                else
-                    val = -minLog;
-//                if (val < -minLog)
-//                    console.log("" + j + " " + i + " " + rawVal + " " + val);
-                var posVal = minLog + val;
-                z[i].push(posVal);
-            } else {
-                z[i][j] = rawVal;
-            }
-            label[i].push(x[j] + ", " + y[i] + " = " + rawVal + "<br>" + bodysite[j] + ", " + gender[j]);
+            var posVal = convertRawValue(rawVal);
+            z[i].push(posVal);
+            label[i].push(x[j] + ", " + y[i] + " = " + rawVal + "<br>" + bodysiteMgLabels[j] + ", " + gender[j]);
         }
     }
 
@@ -121,28 +162,59 @@ var processData = function(data) {
         colorScale.push([colorVal, colors[i]]);
     }
 
-    var traces = [{
-        x: x,
-        y: y,
-        z: z,
-        type: 'heatmap',
-        colorscale: 'Jet',
-        //colorscale: colorScale,
-        text: label,
-        hoverinfo: "text",
-        legend: {
-            orientation: "h",
+    var traces = [
+        {
+            x: x,
+            y: y,
+            z: z,
+            type: 'heatmap',
+            colorscale: 'Jet',
+            //colorscale: colorScale,
+            text: label,
+            hoverinfo: "text",
+            legend: {
+                orientation: "h",
+            },
         },
-    }];
+    ];
+
+    if (useManualExtent) {
+        traces[0].zmin = min;
+        traces[0].zmax = max;
+        traces[0].zauto = false;
+    }
+
+
 
     if (logScale) {
         var tickVals = [0, 1, 2, 3, 3.90];
         traces[0].colorbar = {
+            title: "gene copies per microbial genome",
+            titleside: "right",
             tick0: 0,
             tickmode: "array",
             tickvals: tickVals,
             ticktext: ["<1:10K", "1:1K", "1:100", "1:10", "1:1"],
         };
+    }
+
+    var shapes = [];
+    if (regions.length > 0) {
+        for (var i in regions) {
+            var region = regions[i];
+            var shape = {
+                type: 'rect',
+                xref: 'x',
+                yref: 'paper',
+                x0: region[0]-0.5,
+                y0: 0,
+                x1: region[1]+0.5,
+                y1: -0.02,
+                fillcolor: mgColors[i % mgColors.length],
+                line: { width: 0, },
+            };
+            shapes.push(shape);
+        }
     }
 
     var layout = {
@@ -151,13 +223,15 @@ var processData = function(data) {
         height: 700,
         xaxis: {
             tickangle: -45,
-            title: "Metagenome",
+//            title: "Metagenome",
+            showticklabels: false,
         },
         yaxis: {
             title: "Cluster Number",
                 autorange: "reversed",
                 type: "category",
         },
+        shapes: shapes,
     };
 
     Plotly.newPlot('plot', traces, layout);
@@ -167,6 +241,11 @@ var processData = function(data) {
 
 
 doFormPost();
+document.body.addEventListener('keyup', function(e) {
+    if (e.keyCode == 13) {
+        doFormPost();
+    }
+});
 
 
 function resetFilter() {
@@ -185,6 +264,7 @@ function doFormPost() {
     var parms = new FormData;
     parms.append("id", Id);
     parms.append("key", Key);
+    parms.append("res", ResType);
 
     var clusterList = document.getElementById("cluster-filter").value;
     if (clusterList) {
