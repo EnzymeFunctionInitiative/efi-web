74f763581cd29dd418ad9fa60985d6e90bcc7182
Nils Oberg <noberg@illinois.edu>
2018-10-26 16:46:38 -0500

    Updated tutorial and minor web ui adjustments.

--- shortbred/html/tutorial/intro.inc.php
+++ shortbred/html/tutorial/intro.inc.php
@@ -62,9 +62,9 @@ readily extended to additional enzyme families and microbial communities."
 <p>
 In its original form, the CGFP pipeline described by Balskus and Huttenhower 
 (<a href="https://bitbucket.org/biobakery/cgfp/overview">https://bitbucket.org/biobakery/cgfp/overview</a>)
-requires both knowledge of Unix 
-command line scripts and access to a computer cluster. <b>The EFI-CGFP web tool 
-was developed to democratize the use of CGFP by experimentalists by making it 
+required both knowledge of Unix 
+command line programs and access to a computer cluster. <b>The EFI-CGFP web tool 
+was developed to "democratize" the use of CGFP by experimentalists by making it 
 both accessible and "user friendly".</b>
 </p>
 
