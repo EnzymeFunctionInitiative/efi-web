92683ca02fb37fb0585a3c3822e30d92cdfdbb9c
Nils Oberg <noberg@illinois.edu>
2018-12-10 14:17:16 -0600

    Fixed bug in schema SQL.

--- efi-gnt/sql/all_13_json.sql
+++ efi-gnt/sql/all_13_json.sql
@@ -1,4 +1,4 @@
 
-ADD COLUMN gnn_params TEXT AFTER gnn_gnt_source_id;
-ADD COLUMN gnn_results TEXT AFTER gnn_params;
+ALTER TABLE gnn ADD COLUMN gnn_params TEXT AFTER gnn_gnt_source_id;
+ALTER TABLE gnn ADD COLUMN gnn_results TEXT AFTER gnn_params;
 
