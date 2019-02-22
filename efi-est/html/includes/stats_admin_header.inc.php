d62f5b3123574e43d389b22d0baf97274377d33b
Nils Oberg <noberg@illinois.edu>
2019-02-22 13:47:24 -0600

    Reorganized and improved EFI-EST admin pages.

--- efi-est/html/includes/stats_admin_header.inc.php
+++ efi-est/html/includes/stats_admin_header.inc.php
@@ -1,8 +1,16 @@
+<?php
+$queue_text = "";
+if (isset($NumWaitingJobs) && isset($NumRunningJobs)) {
+    $queue_text = " (NEW $NumWaitingJobs/RUN $NumRunningJobs)";
+}
+?>
+
 <!DOCTYPE html>
 <html lang='en'>
 <head>
 <title>EFI-EST Statistics</title>
     <link rel="stylesheet" type="text/css" href="/bs/css/bootstrap.min.css">
+    <script type="text/javascript" src="/js/jquery-3.2.1.min.js"></script>
 <style>
 /*.families-col {
     max-width: 200px;
@@ -19,23 +27,21 @@
 
 <body style='padding-top: 60px;'>
 <nav class="navbar navbar-inverse navbar-fixed-top">
-	<div class='container'>
-		
-		<div class='navbar-header'>
-			<a class='navbar-brand' href='../'><?php echo __TITLE__; ?></a>
-		</div>	
-                <div id='navbar' class='collapse navbar-collapse'>
-			<ul class='nav navbar-nav'>
-                                        <li><a href='index.php'>Generate Stats</a></li>
-					<li><a href='analysis_stats.php'>Analysis Stats</a></li>
-                                        <li><a href='generate.php'>Generate Jobs</a></li>
-					<li><a href='analyse.php'>Analyse Jobs</a></li>
-					<li><a href='databases.php'>Databases</a></li>
-					<li><a href='queue.php'>Queue Status</a></li>
-                        </ul>
-
-                </div>
-	</div>
+    <div class='container'>
+        <div class='navbar-header'>
+            <a class='navbar-brand' href='../'><?php echo __TITLE__; ?></a>
+        </div>    
+        <div id='navbar' class='collapse navbar-collapse'>
+            <ul class='nav navbar-nav'>
+                <li><a href='index.php'>Generate Stats</a></li>
+                <li><a href='index.php?job-type=analysis'>Analysis Stats</a></li>
+                <li><a href='jobs.php?job-type=generate'>Generate Jobs</a></li>
+                <li><a href='jobs.php?job-type=analysis'>Analysis Jobs</a></li>
+                <li><a href='databases.php'>Databases</a></li>
+                <li><a href='queue.php'>Queue Status<?php echo $queue_text; ?></a></li>
+            </ul>
+        </div>
+    </div>
 </nav>
 
 <div class='container' style="width:100%;padding:40px">
