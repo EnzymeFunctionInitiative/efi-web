08913df07df36a245abfd3f3605fbc04bbbabf09
Nils Oberg <noberg@illinois.edu>
2019-02-22 13:47:24 -0600

    Reorganized and improved EFI-EST admin pages.

--- efi-est/html/admin/stats_graph.php
+++ efi-est/html/admin/stats_graph.php
@@ -12,7 +12,6 @@ if (isset($_GET['year'])) {
 }
 if (isset($_GET['month'])) {
     $month = $_GET['month'];
-
 }
 
 $user_id = 0;
@@ -33,13 +32,24 @@ if ($graph_type == 'analysis_daily_jobs') {
     $yaxis = "count";
     $title = "Daily Jobs - " . date("F", mktime(0, 0, 0, $month, 10)) . " - " . $year;
     custom_graph::bar_graph($data,$xaxis,$yaxis,$title);
-}
-
-if ($graph_type == 'generate_daily_jobs') {
+} elseif ($graph_type == 'generate_daily_jobs') {
     $data = efi_statistics::get_generate_daily_jobs($db,$month,$year);
     $xaxis = "day";
     $yaxis = "count";
     $title = "Daily Jobs - " . date("F", mktime(0, 0, 0, $month, 10)) . " - " . $year;
+    custom_graph::bar_graph($data,$xaxis,$yaxis,$title);
+} elseif ($graph_type == 'generate_monthly') {
+    $raw_data = efi_statistics::num_generate_per_month($db);
+    $data = array();
+    foreach ($raw_data as $row) {
+        $month = substr($row['month'], 0, 3);
+        $year = $row['year'];
+        $count = $row['count'];
+        array_push($data, array("month" => "$month $year", "count" => $count));
+    }
+    $xaxis = "month";
+    $yaxis = "count";
+    $title = "Monthly Jobs";
     custom_graph::bar_graph($data,$xaxis,$yaxis,$title);
 }
 
