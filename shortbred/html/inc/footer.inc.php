00980698bf3ebe1ab7d2067063db7428838bf8ac
Nils Oberg <noberg@illinois.edu>
2019-03-26 15:05:27 -0500

    Centralized footer code. Added a button style. Fixed dashboard bug.

--- shortbred/html/inc/footer.inc.php
+++ shortbred/html/inc/footer.inc.php
@@ -1,14 +1,3 @@
-<?php
-
-$feedbackMessage = "Need help or have suggestions or comments?   Please click here.";
-
-if ((isset($Is404Page) && $Is404Page) || (isset($IsExpiredPage) && $IsExpiredPage)) {
-    echo "</div>";
-    if (isset($Is404Page) && $Is404Page)
-        $feedbackMessage = "Please click here to report this.";
-}
-
-?>
 
 <p>
 This site uses the CGFP-ShortBRED programs (<a href="https://bitbucket.org/biobakery/cgfp/src">https://bitbucket.org/biobakery/cgfp/src</a>
@@ -56,20 +45,5 @@ A framework for human microbiome research.
 
 <hr>
 
-            <p><center>If you use the EFI web tools, please <a href="../#citeus">cite us</a>.</center></p>
-
-            <p class="suggestions">
-                <a href="http://enzymefunction.org/content/sequence-similarity-networks-tool-feedback" target="_blank"><?php echo $feedbackMessage; ?></a>
-            </p>
-        </div> <!-- content_holder -->
-
-        <div class="footer_container">
 <?php include("../../html/inc/shared_footer.inc.php"); ?>
-        </div>
-    </div> <!-- container -->
-
-<?php include("../../html/inc/global_login.inc.php"); ?>
-
-</body>
-</html>
 
