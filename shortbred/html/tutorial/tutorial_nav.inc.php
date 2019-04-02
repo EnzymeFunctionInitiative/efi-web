1012bb42d44d0bb46f2accadbbd930b56465129e
Nils Oberg <noberg@illinois.edu>
2019-04-02 17:09:36 -0500

    Redid CGFP landing page tutorial and moved old contents to new tutorial page.

--- shortbred/html/tutorial/tutorial_nav.inc.php
+++ shortbred/html/tutorial/tutorial_nav.inc.php
@@ -2,8 +2,9 @@
 <div class="tutorial-nav">
 <ul>
 	<li><a href="index.php">Start EFI-CGFP</a></li>
-	<li><a href="index.php#tutorial">Introduction to CGFP</a></li>
-	<li><a href="tutorial_overview.php">Overview of Web Tool and Input Parameters</a></li>
+	<li><a href="tutorial_intro.php">Introduction to CGFP</a></li>
+	<li><a href="tutorial_overview.php">Overview of Web Tool</a></li>
+	<li><a href="tutorial_input.php">Input Parameters</a></li>
 	<li><a href="tutorial_quantify.php">Marker Quantification</a></li>
     <li><a href="tutorial_results.php">Quantification and Marker Abundance Results</a></li>
     <li><a href="tutorial_reading.php">Recommended Reading</a></li>
