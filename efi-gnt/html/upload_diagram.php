68a95af3716fc882ab5afa37c8f409eef86897b0
Nils Oberg <noberg@illinois.edu>
2019-04-05 15:33:10 -0500

    Minor bug fixes and cache fix.

--- efi-gnt/html/upload_diagram.php
+++ efi-gnt/html/upload_diagram.php
@@ -8,44 +8,41 @@ $message = "";
 $valid = 0;
 $cookieInfo = "";
 
-if (isset($_POST['submit'])) {
+$valid = 1;
+$file_type = "";
 
-    $valid = 1;
-    $file_type = "";
+if (isset($_FILES['file'])) {
+    $file_type = strtolower(pathinfo($_FILES['file']['name'],PATHINFO_EXTENSION));
+}
 
-    if (isset($_FILES['file'])) {
-        $file_type = strtolower(pathinfo($_FILES['file']['name'],PATHINFO_EXTENSION));
-    }
+if (!isset($_FILES['file'])) {
+    $valid = 0;
+    $message .= "<br><b>Please select a file to upload</b>";
+}
+elseif (isset($_FILES['file']['error']) && ($_FILES['file']['error'] != 0)) {
+    $valid = 0;
+    $message .= "<br><b>Error uploading file: " . functions::get_upload_error($_FILES['file']['error']) . "</b>";
+}
+elseif (!functions::is_valid_diagram_file_type($file_type)) {
+    $valid = 0;
+    $message .= "<br><b>Invalid filetype ($file_type).  The file has to be an " . settings::get_valid_diagram_file_types() . " filetype.</b>";
+}
 
-    if (!isset($_FILES['file'])) {
-        $valid = 0;
-        $message .= "<br><b>Please select a file to upload</b>";
-    }
-    elseif (isset($_FILES['file']['error']) && ($_FILES['file']['error'] != 0)) {
-        $valid = 0;
-        $message .= "<br><b>Error uploading file: " . functions::get_upload_error($_FILES['file']['error']) . "</b>";
-    }
-    elseif (!functions::is_valid_diagram_file_type($file_type)) {
-        $valid = 0;
-        $message .= "<br><b>Invalid filetype ($file_type).  The file has to be an " . settings::get_valid_diagram_file_types() . " filetype.</b>";
-    }
+if (!functions::verify_email($_POST['email'])) {
+    $valid = 0;
+    $message .= "<br><b>Please verify your e-mail address</b>";
+}
 
-    if (!functions::verify_email($_POST['email'])) {
-        $valid = 0;
-        $message .= "<br><b>Please verify your e-mail address</b>";
-    }
+$email = $_POST['email'];
+$jobGroup = isset($_POST['job-group']) ? $_POST['job-group'] : '';
 
-    $email = $_POST['email'];
-    $jobGroup = isset($_POST['job-group']) ? $_POST['job-group'] : '';
-
-    if ($valid) {
-        $arrowInfo = diagram_jobs::create_file($db, $email, $_FILES['file']['tmp_name'], $_FILES['file']['name'], $jobGroup);
-        if ($arrowInfo === false) {
-            $valid = false;
-        } else {
-            $id = $arrowInfo['id'];
-            $key = $arrowInfo['key'];
-        }
+if ($valid) {
+    $arrowInfo = diagram_jobs::create_file($db, $email, $_FILES['file']['tmp_name'], $_FILES['file']['name'], $jobGroup);
+    if ($arrowInfo === false) {
+        $valid = false;
+    } else {
+        $id = $arrowInfo['id'];
+        $key = $arrowInfo['key'];
     }
 }
 
