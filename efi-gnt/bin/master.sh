7a483ecab0a5c307539627e8f4a2fbccd1055cbe
Nils Oberg <noberg@illinois.edu>
2019-07-26 16:18:53 -0500

    Prevent duplicate copies of the master script from executing.

--- efi-gnt/bin/master.sh
+++ efi-gnt/bin/master.sh
@@ -3,6 +3,7 @@ DATE=$(date +"%Y-%m-%d %H:%M:%S")
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "$DATE: Start EFI-GNT Master script"
 
+if [[ "`ps -ef | grep $0 | grep -v grep | wc -l`" -gt 3 ]]; then echo "Already running; exiting"; exit; fi
 #export EFI_DEBUG=1
 
 echo "Checking for GNN completion"
