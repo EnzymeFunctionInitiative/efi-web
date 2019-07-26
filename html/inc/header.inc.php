68990f09409bfd07ff6d48e65b94462d815f0727
Nils Oberg <noberg@illinois.edu>
2019-07-26 16:20:33 -0500

    Changed the way additional styles/stylesheets get added to the header.

--- html/inc/header.inc.php
+++ html/inc/header.inc.php
@@ -1,7 +1,11 @@
 <?php
 
 $Title = "Enzyme Function Initiative Tools";
-$StyleAdditional = array('<link rel="stylesheet" type="text/css" href="css/main.css">');
+$extraStyle = '<link rel="stylesheet" type="text/css" href="css/main.css">';
+if (isset($StyleAdditional))
+    array_push($StyleAdditional, $extraStyle);
+else
+    $StyleAdditional = array($extraStyle);
 $JsAdditional = array('<script src="js/main.js" type="text/javascript"></script>');
 $BannerImagePath = "images/efi_logo.png";
 
