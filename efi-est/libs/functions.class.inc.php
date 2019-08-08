d255ae1f3b605a540c70fbe7d913f6357fcc605d
Nils Oberg <noberg@illinois.edu>
2019-08-08 12:02:19 -0500

    Removed constant.

--- efi-est/libs/functions.class.inc.php
+++ efi-est/libs/functions.class.inc.php
@@ -6,7 +6,7 @@ class functions {
     //Possible errors when you upload a file
     private static $upload_errors = array(
         1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini.',
-        2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.',
+        2 => 'The uploaded file exceeds the maximu file size.',
         3 => 'The uploaded file was only partially uploaded.',
         4 => 'No file was uploaded.',
         6 => 'Missing a temporary folder.',
