43978cc7d1920e5432103e81b7ff5c845fabd2cd
Nils Oberg <noberg@illinois.edu>
2020-09-24 16:25:13 -0500

    Added internal debug scripts.

---
+++ html/util/id_list.php
@@ -0,0 +1,25 @@
+
+<!doctype html>
+<html>
+<head>
+    <title>ID list sampler</title>
+</head>
+
+<body>
+
+<form action="do_sample.php" method="POST">
+
+<textarea name="list" cols="30" rows="50"></textarea>
+
+<br><br>
+
+<input type="text" name="fraction" value="1" /> Fraction<br><br>
+<button type="submit">Submit</button>
+
+</form>
+
+</body>
+
+</html>
+
+
