681285e049c92e095e0c027431fae5dd36da4f37
David Slater <dslater@illinois.edu>
2020-10-31 15:08:28 -0500

    Added .travis.yml Added travis build logo to README.md Removed drop tables, engine, and starting auto_increment from sql/01_COMPLETE_ALL_TOOLS.sql

--- sql/01_COMPLETE_ALL_TOOLS.sql
+++ sql/01_COMPLETE_ALL_TOOLS.sql
@@ -3,18 +3,16 @@
 -- Table structure for table `PFAM_clans`
 --
 
-DROP TABLE IF EXISTS `PFAM_clans`;
 CREATE TABLE `PFAM_clans` (
   `pfam_id` varchar(24) DEFAULT NULL,
   `clan_id` varchar(24) DEFAULT NULL,
   KEY `clan_id_Index` (`clan_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `analysis`
 --
 
-DROP TABLE IF EXISTS `analysis`;
 CREATE TABLE `analysis` (
   `analysis_id` int(11) NOT NULL AUTO_INCREMENT,
   `analysis_generate_id` int(11) DEFAULT NULL,
@@ -33,13 +31,12 @@ CREATE TABLE `analysis` (
   `analysis_cdhit_opt` varchar(10) DEFAULT NULL,
   `analysis_params` text DEFAULT NULL,
   PRIMARY KEY (`analysis_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=53831 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `analysis_example`
 --
 
-DROP TABLE IF EXISTS `analysis_example`;
 CREATE TABLE `analysis_example` (
   `analysis_id` int(11) NOT NULL AUTO_INCREMENT,
   `analysis_generate_id` int(11) DEFAULT NULL,
@@ -58,26 +55,24 @@ CREATE TABLE `analysis_example` (
   `analysis_cdhit_opt` varchar(10) DEFAULT NULL,
   `analysis_params` text DEFAULT NULL,
   PRIMARY KEY (`analysis_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=35896 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `applications`
 --
 
-DROP TABLE IF EXISTS `applications`;
 CREATE TABLE `applications` (
   `app_name` varchar(255) DEFAULT NULL,
   `app_email` varchar(255) DEFAULT NULL,
   `app_institution` varchar(255) DEFAULT NULL,
   `app_body` text DEFAULT NULL,
   `app_status` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `bigscape`
 --
 
-DROP TABLE IF EXISTS `bigscape`;
 CREATE TABLE `bigscape` (
   `bigscape_id` int(11) NOT NULL AUTO_INCREMENT,
   `bigscape_diagram_id` int(11) DEFAULT NULL,
@@ -88,13 +83,12 @@ CREATE TABLE `bigscape` (
   `bigscape_time_started` datetime DEFAULT NULL,
   `bigscape_time_completed` datetime DEFAULT NULL,
   PRIMARY KEY (`bigscape_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=278 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `db_version`
 --
 
-DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `db_version_id` int(11) NOT NULL AUTO_INCREMENT,
   `db_version_date` varchar(255) DEFAULT NULL,
@@ -103,13 +97,12 @@ CREATE TABLE `db_version` (
   `db_version_default` tinyint(1) DEFAULT 0,
   `db_version_created` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`db_version_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `diagram`
 --
 
-DROP TABLE IF EXISTS `diagram`;
 CREATE TABLE `diagram` (
   `diagram_id` int(11) NOT NULL AUTO_INCREMENT,
   `diagram_key` varchar(255) DEFAULT NULL,
@@ -124,33 +117,30 @@ CREATE TABLE `diagram` (
   `diagram_params` text DEFAULT NULL,
   `diagram_results` text DEFAULT NULL,
   PRIMARY KEY (`diagram_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2257 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `email`
 --
 
-DROP TABLE IF EXISTS `email`;
 CREATE TABLE `email` (
   `email` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `email_status`
 --
 
-DROP TABLE IF EXISTS `email_status`;
 CREATE TABLE `email_status` (
   `email` varchar(255) NOT NULL,
   `opt_in` tinyint(1) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+);
 
 --
 -- Table structure for table `family_info`
 --
 
-DROP TABLE IF EXISTS `family_info`;
 CREATE TABLE `family_info` (
   `family` varchar(10) NOT NULL,
   `short_name` varchar(50) DEFAULT NULL,
@@ -160,13 +150,12 @@ CREATE TABLE `family_info` (
   `num_uniref90_members` int(11) DEFAULT NULL,
   PRIMARY KEY (`family`),
   KEY `family_Index` (`family`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `generate`
 --
 
-DROP TABLE IF EXISTS `generate`;
 CREATE TABLE `generate` (
   `generate_id` int(11) NOT NULL AUTO_INCREMENT,
   `generate_email` varchar(255) DEFAULT NULL,
@@ -184,13 +173,12 @@ CREATE TABLE `generate` (
   `generate_params` text NOT NULL,
   `generate_parent_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`generate_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=43644 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `generate_example`
 --
 
-DROP TABLE IF EXISTS `generate_example`;
 CREATE TABLE `generate_example` (
   `generate_id` int(11) NOT NULL AUTO_INCREMENT,
   `generate_email` varchar(255) DEFAULT NULL,
@@ -208,13 +196,12 @@ CREATE TABLE `generate_example` (
   `generate_params` text NOT NULL,
   `generate_parent_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`generate_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=29550 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `gnn`
 --
 
-DROP TABLE IF EXISTS `gnn`;
 CREATE TABLE `gnn` (
   `gnn_id` int(11) NOT NULL AUTO_INCREMENT,
   `gnn_email` varchar(255) DEFAULT NULL,
@@ -230,13 +217,12 @@ CREATE TABLE `gnn` (
   `gnn_params` text DEFAULT NULL,
   `gnn_results` text DEFAULT NULL,
   PRIMARY KEY (`gnn_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6517 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `gnn_example`
 --
 
-DROP TABLE IF EXISTS `gnn_example`;
 CREATE TABLE `gnn_example` (
   `gnn_id` int(11) NOT NULL AUTO_INCREMENT,
   `gnn_email` varchar(255) DEFAULT NULL,
@@ -252,26 +238,24 @@ CREATE TABLE `gnn_example` (
   `gnn_params` text DEFAULT NULL,
   `gnn_results` text DEFAULT NULL,
   PRIMARY KEY (`gnn_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10443 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `groups`
 --
 
-DROP TABLE IF EXISTS `groups`;
 CREATE TABLE `groups` (
   `group_name` varchar(100) NOT NULL,
   `group_status` varchar(10) DEFAULT NULL,
   `group_time_open` datetime DEFAULT NULL,
   `group_time_closed` datetime DEFAULT NULL,
   PRIMARY KEY (`group_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `identify`
 --
 
-DROP TABLE IF EXISTS `identify`;
 CREATE TABLE `identify` (
   `identify_id` int(11) NOT NULL AUTO_INCREMENT,
   `identify_email` varchar(255) DEFAULT NULL,
@@ -285,13 +269,12 @@ CREATE TABLE `identify` (
   `identify_copy_id` int(11) DEFAULT NULL,
   `identify_params` text DEFAULT NULL,
   PRIMARY KEY (`identify_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=735 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `identify_example`
 --
 
-DROP TABLE IF EXISTS `identify_example`;
 CREATE TABLE `identify_example` (
   `identify_id` int(11) NOT NULL AUTO_INCREMENT,
   `identify_email` varchar(255) DEFAULT NULL,
@@ -305,13 +288,12 @@ CREATE TABLE `identify_example` (
   `identify_copy_id` int(11) DEFAULT NULL,
   `identify_params` text DEFAULT NULL,
   PRIMARY KEY (`identify_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2207 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `quantify`
 --
 
-DROP TABLE IF EXISTS `quantify`;
 CREATE TABLE `quantify` (
   `quantify_id` int(11) NOT NULL AUTO_INCREMENT,
   `quantify_identify_id` int(11) DEFAULT NULL,
@@ -323,13 +305,12 @@ CREATE TABLE `quantify` (
   `quantify_parent_id` int(11) DEFAULT NULL,
   `quantify_params` text DEFAULT NULL,
   PRIMARY KEY (`quantify_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=708 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `quantify_example`
 --
 
-DROP TABLE IF EXISTS `quantify_example`;
 CREATE TABLE `quantify_example` (
   `quantify_id` int(11) NOT NULL AUTO_INCREMENT,
   `quantify_identify_id` int(11) DEFAULT NULL,
@@ -341,24 +322,22 @@ CREATE TABLE `quantify_example` (
   `quantify_parent_id` int(11) DEFAULT NULL,
   `quantify_params` text DEFAULT NULL,
   PRIMARY KEY (`quantify_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2173 DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `user_group`
 --
 
-DROP TABLE IF EXISTS `user_group`;
 CREATE TABLE `user_group` (
   `group_name` varchar(100) NOT NULL,
   `user_id` varchar(255) NOT NULL,
   PRIMARY KEY (`group_name`,`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `user_token`
 --
 
-DROP TABLE IF EXISTS `user_token`;
 CREATE TABLE `user_token` (
   `user_id` varchar(255) DEFAULT NULL,
   `user_email` varchar(255) DEFAULT NULL,
@@ -366,29 +345,27 @@ CREATE TABLE `user_token` (
   `user_action` varchar(10) DEFAULT NULL,
   `user_group` varchar(100) DEFAULT NULL,
   `user_admin` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `job_group`
 --
 
-DROP TABLE IF EXISTS `job_group`;
 CREATE TABLE `job_group` (
   `job_type` varchar(4) NOT NULL DEFAULT "",
   `job_id` int(11) NOT NULL DEFAULT 0,
   `other_id` int(11) DEFAULT NULL,
   `user_group` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`job_type`,`job_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 --
 -- Table structure for table `job_cancel`
 --
 
-DROP TABLE IF EXISTS `job_cancel`;
 CREATE TABLE `job_cancel` (
   `job_type` varchar(4) NOT NULL DEFAULT "",
   `job_process_num` int(11) NOT NULL DEFAULT 0,
   `cancel_status` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`job_type`,`job_process_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
