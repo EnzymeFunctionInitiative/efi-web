c90837db37fc3454cb2480bb7f12ab2efeccb6b2
Nils Oberg <noberg@illinois.edu>
2021-12-21 20:11:24 -0600

    Added missing file.

---
+++ includes/debug_check.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+if (defined("__ENABLE_DEBUG__")) {
+    ini_set("log_errors", 1);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+}
+
