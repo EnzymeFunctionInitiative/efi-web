d65147dbbea199d0d3e8638d1eec7eccca27b2f2
Nils Oberg <noberg@illinois.edu>
2024-11-12 11:25:45 -0600

    Verify user is logged in on all pages

--- html/efi-cgfp/stepc.php
+++ html/efi-cgfp/stepc.php
@@ -1,6 +1,8 @@
 <?php 
 require_once(__DIR__."/../../init.php");
 
+require_once(__BASE_DIR__."/includes/login_check.inc.php");
+
 use \efi\global_functions;
 use \efi\table_builder;
 use \efi\user_auth;
